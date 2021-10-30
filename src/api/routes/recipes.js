e2d19e141e0935a6c890344311f8154a549a50e7
Renan <renan.vamo@gmail.com>
2021-10-30 11:13:49 -0300

    FEAT: Adiciona um novo metodo para buscar todas as receitas

--- src/api/routes/recipes.js
+++ src/api/routes/recipes.js
@@ -4,5 +4,6 @@ const { checkToken } = require('../validations/checkToken');
 // const validateJWT = require('../validations/tokenValidations');
 
 router.post('/', checkToken, recipesController.createRecipe);
+router.get('/');
 
 module.exports = router;
--- src/api/validations/checkToken.js
+++ src/api/validations/checkToken.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 
 const checkToken = (req, res, next) => {
   const token = req.headers.authorization;
-  console.log('oi');
 
   try {
     const decode = jwt.verify(token, SECRET);
