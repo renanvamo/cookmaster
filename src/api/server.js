6888291160de8546f257ecbf838506d73ed07f01
Renan <renan.vamo@gmail.com>
2021-11-01 01:18:14 -0300

    FEAT(imagesRoute): adiciona uma rota para resgatar imagens das receitas

--- src/api/server.js
+++ src/api/server.js
@@ -4,6 +4,7 @@ const app = require('./app');
 const userRoutes = require('./routes/users');
 const loginRouter = require('./routes/login');
 const recipesRouter = require('./routes/recipes');
+const imagesRouter = require('./routes/images');
 const { getErrors } = require('./middlewares/errors');
 
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use(express.static(path.join(__dirname, '..', 'uploads')));
 app.use('/users', userRoutes);
 app.use('/login', loginRouter);
 app.use('/recipes', recipesRouter);
+app.use('/images', imagesRouter);
 
 app.use(getErrors);
 
